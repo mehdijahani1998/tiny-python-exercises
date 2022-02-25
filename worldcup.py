8c8ee9cdda1c0d969fe6f69a939bdf5e5c7ad0a6
mehdijahani <mehdijahani199815@gmail.com>
2022-02-25 22:54:03 +0330

    just renewing stuff

--- worldcup.py
+++ worldcup.py
@@ -1,8 +1,11 @@
-spain = {"wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
-iran = {"wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
-protugal = {"wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
-morroco = {"wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
-teams = [iran, spain, protugal, morroco]
+
+spain = {"name":"Spain","wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
+iran = {"name":"Iran","wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
+portugal = {"name":"Portugal","wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
+morocco = {"name":"Morocco","wins":0 , "loses":0 , "draws":0 ,"goal difference":0 , "points":0}
+
+teams = [iran, spain, portugal, morocco]
+
 def team_win_update(team):
     team.update({"wins":team.get("wins")+1})
     team.update({"points":team.get("points")+3})
@@ -17,10 +20,39 @@ def team_tie_update(team):
 def team_goal_diff_update(team, goals):
     team.update({"goal difference": team.get("goal difference")+goals})
 
-for i in range(4):
-    for j in range(3-i)
-    a,b = list(map(int, input().split("-")))
-    goal_diff = a-b
-    if a == b:
-        
+def compare_points(team):
+    return team.get("points")
+
+def compare_wins (team):
+    return team.get("wins")
+
+def compare_name (team):
+    return ord("z") - ord(team.get("name")[0])
+
+def print_team(team):
+    print("{}  wins:{} , loses:{} , draws:{} , goal difference:{} , points:{}".format(team.get("name"), team.get("wins"), team.get("loses"), team.get("draws"), team.get("goal difference"), team.get("points")))
+
+for i in range(3):
+    for j in range(3-i):
+        a,b = list(map(int, input().split("-")))
+        if a == b:
+            team_tie_update(teams[i])
+            team_tie_update(teams[j+i+1])
+        if a > b:
+            team_win_update(teams[i])
+            team_goal_diff_update(teams[i],a-b)
+            team_lose_update(teams[j+i+1])
+            team_goal_diff_update(teams[j+i+1],b-a)
+        if a < b:
+            team_win_update(teams[j+i+1])
+            team_goal_diff_update(teams[j+i+1],b-a)
+            team_lose_update(teams[i])
+            team_goal_diff_update(teams[i],a-b)
+
+teams.sort(key=lambda x: (compare_points(x), compare_wins(x), compare_name(x)), reverse=True)
+
+for team in teams:
+    print_team(team)
+
+
 
